219364c38a953371b3932cd0d5781c31f82b441c
ipsitadipanweeta <ipsita.dipanweeta56@gmail.com>
2025-07-06 00:38:55 -0400

    test1 commit95

--- src/test/java/tests/api/ApiTests.java
+++ src/test/java/tests/api/ApiTests.java
@@ -2,7 +2,6 @@ package tests.api;
 
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
